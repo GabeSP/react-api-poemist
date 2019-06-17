360494171548ff6b2a45b96b4889f082a24020e7
Gabriel Pereira <ga_dsp@hotmail.com>
2019-06-17 00:57:40 -0300

    add axios and make a call to poemist api

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Header from '../../components/Header/Header'
 import Poems from '../Poems/Poems'
 import style from './style.module.css'
@@ -8,9 +9,35 @@ class App extends Component {
     super();
 
     this.state = {
-      title: 'Poemist API'
+      title: 'Poemist API',
+      poems: []
     }
   }
+
+  componentDidMount() {
+    axios
+    .get('https://www.poemist.com/api/v1/randompoems', {
+      headers: {"Access-Control-Allow-Origin": "*"}
+    })
+    .then(response => {
+       console.log('-->', response)
+      const newPoems = response.data.map(r => {
+        return {
+          title: r.title,
+          name: r.poet.name,
+          url: r.poet.url
+        }
+      })
+
+      const newState = Object.assign({}, this.state, {
+        poems: newPoems
+      })
+
+      this.setState(newState)
+    })
+    .catch(error => console.log(error));
+  }
+
   render () {
     return (
       <div>
